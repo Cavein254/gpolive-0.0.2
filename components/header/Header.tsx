3f194e6adbbc083cbf11b9ef7087da930146565d
johnmuthua <caveomuthama@gmail.com>
2023-09-08 13:17:18 +0300

    initial commit

---
+++ components/header/Header.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { Box, Button, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Link from 'next/navigation';
+import { useState } from 'react';
+import { BsFan } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Overlay from '../overlay/Overlay';
+import { HeaderLinks } from './LinkData';
+import './styles.css';
+const StyledSignUp = styled(Button)`
+  background-color: #ff6600;
+  color: #fff;
+  font-size: 0.8rem;
+  border-radius: 0;
+  '&:hover': {
+    background-color: '#fff';
+    color: '#ff6600';
+  }
+`;
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const linkNavs = HeaderLinks.map((link) => {
+    return (
+      <span key={link.id} className="header-links">
+        <a href={link.path} className="header-link">
+          {link.name}
+        </a>
+      </span>
+    );
+  });
+  return (
+    <Box component="nav">
+      {isOpen && <Overlay isOpen={isOpen} setIsOpen={setIsOpen} />}
+      <Box className="header-container">
+        <Box className="header-icon-container">
+          <GiHamburgerMenu
+            className="header-icon"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </Box>
+        <Box className="header-details">
+          <BsFan className="header-company-logo" />
+          <Typography
+            variant="h4"
+            sx={{
+              fontFamily: 'Merriweather',
+              fontWeight: '900',
+              fontSize: '1.8rem',
+            }}
+          >
+            GPOL
+          </Typography>
+        </Box>
+        <Box className="header-links-container">
+          <Box className="header-links-wrapper">{linkNavs}</Box>
+        </Box>
+        <Box className="header-btns">
+          <Link to="/login" className="header-link">
+            <Button
+              variant="text"
+              className="header-login-btn"
+              sx={{
+                marginRight: '0.4rem',
+                fontSize: '1rem',
+              }}
+            >
+              Log In
+            </Button>
+          </Link>
+          <Link to="/register" className="header-link">
+            <StyledSignUp variant="contained">Sign Up</StyledSignUp>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
