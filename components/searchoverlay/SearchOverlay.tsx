53c874dc2e8611071328ef9f1d612f1a67e3f59c
johnmuthua <caveomuthama@gmail.com>
2023-09-09 15:28:13 +0300

    complete search skeleton

--- components/searchoverlay/SearchOverlay.tsx
+++ components/searchoverlay/SearchOverlay.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, Typography } from "@mui/material"
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, Typography } from "@mui/material"
 import { useState } from "react"
 import { AiOutlineFileWord } from 'react-icons/ai'
 import { BiRecycle } from "react-icons/bi"
@@ -70,7 +70,54 @@ const SearchOverlay = () => {
                             </FormControl>
                             </Box>
                         </Box>
+                        <Box>
+                            <Box>
+                                <GiWorld />
+                                <Typography variant='h5'>Industry</Typography>
+                            </Box>
+                            <Box>
+                            <FormGroup>
+                                <FormControlLabel control={<Checkbox />} label="Consumer" />
+                                <FormControlLabel control={<Checkbox />} label="IT" />
+                                <FormControlLabel control={<Checkbox />} label="Hospitality" />
+                                <FormControlLabel control={<Checkbox />} label="NGO" />
+                                <FormControlLabel control={<Checkbox />} label="Health Care" />
+                                <FormControlLabel control={<Checkbox />} label="Construction" />
+                                <FormControlLabel control={<Checkbox />} label="Transport" />
+                                <FormControlLabel control={<Checkbox />} label="Other" />
+                            </FormGroup>
+                            </Box>
+                        </Box>
+                        <Box>
+                            <Box>
+                                <GiWorld />
+                                <Typography variant='h5'>Job Type</Typography>
+                            </Box>
+                            <Box>
+                                <FormControlLabel control={<Checkbox />} label="Permanent" />
+                                <FormControlLabel control={<Checkbox />} label="Temporary" />
+                                <FormControlLabel control={<Checkbox />} label="Contract" />
+                                <FormControlLabel control={<Checkbox />} label="Internship" />
+                            </Box>
+                        </Box>
+                        <Box>
+                            <Box>
+                                <GiWorld />
+                                <Typography variant='h5'>Job Engagement</Typography>
+                            </Box>
+                            <Box>
+                                <FormControlLabel control={<Checkbox />} label="Full Time" />
+                                <FormControlLabel control={<Checkbox />} label="Part Time" />
+                                
+                            </Box>
+                        </Box>
+
                     </Box>
+                </Box>
+                <Box>
+                    <Button variant="contained">
+                        Show Results
+                    </Button>
                 </Box>
             </Box>
         </form>
